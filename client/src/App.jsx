3fd9d21170640dd9ffd16f049d2d0e4196b5d91c
Trick$$tar <48604327+DXpz@users.noreply.github.com>
2025-01-16 09:00:15 -0600

    Interfaz 0.0.0.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="app">
+        <div className="menu">
+          <h2>HeavyHaulBoard</h2>
+          <ul>
+            <li>Inicio</li>
+            <li>Dashboard</li>
+            <li>Configuración</li>
+            <li>Perfil</li>
+            <li>Cerrar Sesión</li>
+          </ul>
+        </div>
+        <div className="content">
+          <div className='header'><h1>HeavyHaulBoard</h1></div>
+          <div className='viaje-sel'>haz selecionado tal carga</div>
+          <div className='tabla-cont'>
+            <div className='resultados'>100 resultados</div>
+            <div className='viajes'>
+              <table>
+                <tr>
+                  <td>Origen</td>
+                  <td>Destino</td>
+                  <td>Dia de carga</td>
+                  <td>Tamaño / Peso</td>
+                  <td>Tipo de equipo</td>
+                  <td className='detalles'>Detalles</td>
+                </tr>
+                <tr>
+                  <td>Raza</td>
+                  <td>Jack Russell</td>
+                  <td>Caniche</td>
+                  <td>Perro callejero</td>
+                  <td>Cocker Spaniel</td>
+                  <td>Cocker Spaniel</td>
+                </tr>
+                <tr>
+                  <td>Edad</td>
+                  <td>16</td>
+                  <td>9</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>Cocker Spaniel</td>
+                </tr>
+                <tr>
+                  <td>Propietario</td>
+                  <td>Suegra</td>
+                  <td>Yo</td>
+                  <td>Yo</td>
+                  <td>Cuñada</td>
+                  <td>Cocker Spaniel</td>
+                </tr>
+                <tr>
+                  <td>Hábitos alimentarios</td>
+                  <td>Come las sobras de todos</td>
+                  <td>Mordisquea la comida</td>
+                  <td>Come en abundancia</td>
+                  <td>Come hasta que revienta</td>
+                  <td>Cocker Spaniel</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
